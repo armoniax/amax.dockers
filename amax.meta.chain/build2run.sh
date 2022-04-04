ecd7a609eb3c00492baf008b7c6a423ad9272232
masteray <masteraychen@gmail.com>
2022-04-04 16:56:03 +0800

    spearate docker compose

--- amax.meta.chain/build2run.sh
+++ amax.meta.chain/build2run.sh
@@ -5,4 +5,4 @@ HOME_DIR=/opt/data/amax_$NET
 mkdir -p $HOME_DIR $HOME_DIR/data $HOME_DIR/logs
 
 svc=amond-$NET
-podman-compose up $svc --build -d
+podman-compose up -f ./docker-compose.${NET}.yml --build -d
