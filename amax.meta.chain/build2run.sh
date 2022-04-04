db14dfe3515ee80ec9a1635820de8649fe7e54a5
masteray <masteraychen@gmail.com>
2022-04-04 16:43:23 +0800

    update

--- amax.meta.chain/build2run.sh
+++ amax.meta.chain/build2run.sh
@@ -1,4 +1,9 @@
-svc=$1
-[ -z "$svc" ] && svc=amnod-testnet
+NET=$1
+[ -z "$NET" ] && svc=testnet
 
+HOME_DIR=/opt/data/amax_$NET
+mkdir -p $HOME_DIR
+cd $HOME_DIR && mkdir -p data logs
+
+svc=amond-$NET
 podman-compose up $svc --build -d 
