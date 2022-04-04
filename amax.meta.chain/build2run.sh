377ca63d2b7f55466a2b97a9a443dc9a7c9b6814
masteray <masteraychen@gmail.com>
2022-04-04 18:11:11 +0800

    update dockerfile

--- amax.meta.chain/build2run.sh
+++ amax.meta.chain/build2run.sh
@@ -1,10 +1,10 @@
 NET=$1
 [ -z "$NET" ] && NET=testnet
 
-SRC_DIR=env_$NET
-HOME_DIR=/opt/data/amax_$NET
-mkdir -p $HOME_DIR $HOME_DIR/data $HOME_DIR/logs
-cp -r ./${SRC_DIR}/bin  $HOME_DIR/
-cp -r ./${SRC_DIR}/conf $HOME_DIR/
+SRC_DIR=./env_$NET
+NOD_DIR=/opt/data/amax_$NET
+mkdir -p $NOD_DIR $NOD_DIR/data $NOD_DIR/logs
+cp -r ${SRC_DIR}/bin  $NOD_DIR/
+cp -r ${SRC_DIR}/conf $NOD_DIR/
 
-podman-compose -f ./${SRC_DIR}/docker-compose.yml up --build -d
+podman-compose -f ${SRC_DIR}/docker-compose.yml up --build -d
