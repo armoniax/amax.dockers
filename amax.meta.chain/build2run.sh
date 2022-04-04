bd3641fb7e3a259b3788785bce7d9e7b7075a566
masteray <masteraychen@gmail.com>
2022-04-04 16:58:35 +0800

    update

--- amax.meta.chain/build2run.sh
+++ amax.meta.chain/build2run.sh
@@ -5,4 +5,4 @@ HOME_DIR=/opt/data/amax_$NET
 mkdir -p $HOME_DIR $HOME_DIR/data $HOME_DIR/logs
 
 svc=amond-$NET
-podman-compose up -f ./docker-compose.${NET}.yml --build -d
+podman-compose -f ./docker-compose.${NET}.yml up --build -d
