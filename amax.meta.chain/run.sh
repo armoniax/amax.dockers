501925f3d23dcdbcf69d7811a2ea6884a1d8655e
masteray <masteraychen@gmail.com>
2022-04-10 16:04:25 +0800

    update devnet

--- amax.meta.chain/run.sh
+++ amax.meta.chain/run.sh
@@ -11,7 +11,13 @@ set -a
 source ${SRC_DIR}/.env
 podman-compose up -d
 
-if [ "$NET" = "mainnet" ]; then
+if   [ "$NET" = "mainnet" ]; then
     sudo iptables -I INPUT -p tcp -m tcp --dport 9806 -j ACCEPT
     sudo iptables -I INPUT -p tcp -m tcp --dport 8888 -j ACCEPT
+elif [ "$NET" = "testnet" ]; then
+    sudo iptables -I INPUT -p tcp -m tcp --dport 19806 -j ACCEPT
+    sudo iptables -I INPUT -p tcp -m tcp --dport 18888 -j ACCEPT
+elif [ "$NET" = "devnet" ]; then
+    sudo iptables -I INPUT -p tcp -m tcp --dport 29806 -j ACCEPT
+    sudo iptables -I INPUT -p tcp -m tcp --dport 28888 -j ACCEPT
 fi
