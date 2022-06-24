508d37755980d79f081847d7f8fea7278f281c5f
masteray <masteraychen@gmail.com>
2022-06-24 14:22:30 +0800

    refactor

--- amax.meta.chain/run.sh
+++ amax.meta.chain/run.sh
@@ -6,26 +6,25 @@ BP=$3
 [ -z "$His" ] && His="na"
 [ -z "$BP" ] && BP="na"
 
-cd $NET
 set -a
-source ./.env
+source "./${NET}/.env"
 
-NOD_DIR=/opt/data/amax_$NET
-mkdir -p $NOD_DIR $NOD_DIR/data $NOD_DIR/logs
+NOD_DIR="${NODE_HOME}/amax_${NET}"
+mkdir -p $NOD_DIR/conf $NOD_DIR/data $NOD_DIR/logs
 
-cp -r ../bin $NOD_DIR/
-cp -r ./conf $NOD_DIR/
-cp ../config.ini $NOD_DIR/conf/
+cp -r ./bin $NOD_DIR/
+cp $NET/genesis.json $NOD_DIR/
+cp ./config.ini $NOD_DIR/conf/
 
 # copy conf node info into config
-cat "../conf_node_${NET}.ini" >> $NOD_DIR/conf/config.ini
+cat "./${NET}/conf_node.ini" >> $NOD_DIR/conf/config.ini
 
 if  [ "$His" == "his" ]; then
-    cat ../conf_plugin_history.ini >> $NOD_DIR/conf/config.ini
+    cat ./conf_plugin_history.ini >> $NOD_DIR/conf/config.ini
 fi
 
 if  [ "$BP" == "bp" ]; then
-    cat ../conf_plugin_bp.ini >> $NOD_DIR/conf/config.ini
+    cat ./conf_plugin_bp.ini >> $NOD_DIR/conf/config.ini
 fi
 
 #podman-compose up -d
@@ -34,9 +33,11 @@ docker-compose up -d
 if   [ "$NET" = "mainnet" ]; then
     sudo iptables -I INPUT -p tcp -m tcp --dport 9806 -j ACCEPT
     sudo iptables -I INPUT -p tcp -m tcp --dport 8888 -j ACCEPT
+
 elif [ "$NET" = "testnet" ]; then
     sudo iptables -I INPUT -p tcp -m tcp --dport 19806 -j ACCEPT
     sudo iptables -I INPUT -p tcp -m tcp --dport 18888 -j ACCEPT
+    
 elif [ "$NET" = "devnet" ]; then
     sudo iptables -I INPUT -p tcp -m tcp --dport 29806 -j ACCEPT
     sudo iptables -I INPUT -p tcp -m tcp --dport 28888 -j ACCEPT
