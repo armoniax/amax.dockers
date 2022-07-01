d47c2ba1b440f14e4a324407e16754f34be97d7f
masteray <masteraychen@gmail.com>
2022-07-01 19:41:25 +0800

    update

--- amax.meta.chain/run-amnod.sh
+++ amax.meta.chain/run-amnod.sh
@@ -1,36 +1,46 @@
 NET=$1
-His=$2
-BP=$3
 
-[ -z "$NET" ] && NET=devnet
-[ -z "$His" ] && His="na"
-[ -z "$BP"  ] && BP="na"
+CONF_DIR="~/.amax_$NET"
+NOD_DIR="${NODE_HOME}/amax_${NET}"
+DEST_CONF="${NOD_DIR}/conf/config.ini"
 
-cd "amnod_$NET"
+mkdir -p $CONF_DIR $NOD_DIR/conf $NOD_DIR/data $NOD_DIR/logs
+
+cd ./amnod
+[ ! -f $CONF_DIR/amnod.conf ] && cp ./$NET/amnod.env $CONF_DIR/
 
 set -a
-source ./.env
+source $CONF_DIR/amnod.env
 
-NOD_DIR="${NODE_HOME}/amax_${NET}"
-mkdir -p $NOD_DIR/conf $NOD_DIR/data $NOD_DIR/logs
-
-cp -r ../bin $NOD_DIR/
-cp ../config.ini $NOD_DIR/conf/
-cp ./genesis.json $NOD_DIR/conf/
+cp -r   ./bin                   $NOD_DIR/
+cp      ./genesis.json          $NOD_DIR/conf/
+cp      ./conf/conf_base.ini    $DEST_CONF
 
 # copy conf node info into config
-cat "./conf_node.ini" >> $NOD_DIR/conf/config.ini
+echo " " >> $DEST_CONF
+echo "#### Node base conf: " >> $DEST_CONF
+cat ./conf/conf_node.ini >> $DEST_CONF
 
-if  [ "$His" == "his" ]; then
-    cat ../conf_plugin_history.ini >> $NOD_DIR/conf/config.ini
+if  [ "${history_plugin}" == "true" ]; then
+    echo " " >> $DEST_CONF
+    echo "#### History plugin conf: " >> $DEST_CONF
+    cat ./conf/conf_plugin_history.ini >> $DEST_CONF
 fi
 
-if  [ "$BP" == "bp" ]; then
-    cat ../conf_plugin_bp.ini >> $NOD_DIR/conf/config.ini
+if  [ "${state_plugin}" == "true" ]; then
+    echo " " >> $DEST_CONF
+    echo "#### State plugin conf: " >> $DEST_CONF
+    cat ./conf/conf_plugin_state.ini >> $DEST_CONF
 fi
 
-#podman-compose up -d
-docker-compose up -d
+if  [ "${bp_plugin}" == "true" ]; then
+    echo " " >> $DEST_CONF
+    echo "#### Block producer plugin conf: " >> $DEST_CONF
+    cat ./conf/conf_plugin_bp.ini >> $DEST_CONF
+fi
+
+cd $NET && docker-compose up -d
+#cd $NET && podman-compose up -d
 
 if   [ "$NET" = "mainnet" ]; then
     sudo iptables -I INPUT -p tcp -m tcp --dport 9806 -j ACCEPT
