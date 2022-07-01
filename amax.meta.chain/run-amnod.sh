5dc212b80e5f62f4591b4690eb17c22d185c39a2
masteray <masteraychen@gmail.com>
2022-07-02 00:35:06 +0800

    update conf

--- amax.meta.chain/run-amnod.sh
+++ amax.meta.chain/run-amnod.sh
@@ -3,39 +3,43 @@ NET=$1
 CONF_DIR=~/.amax_$NET
 mkdir -p $CONF_DIR
 ls -la $CONF_DIR
-[ ! -f $CONF_DIR/amnod.conf ] && cp ./amnod/$NET/amnod.env $CONF_DIR/
+[ ! -f $CONF_DIR/amnod.conf ] && \
+    cp ./amnod/$NET/amnod.env $CONF_DIR/ && \
+    cp -r ./amnod/conf $CONF_DIR/ && \
+    cp -r ./amnod/bin $CONF_DIR/ && \
+    cp ./amnod/$NET/genesis.json $CONF_DIR/
+
 set -a && source $CONF_DIR/amnod.env
 
-NOD_DIR="${NODE_HOME}/amax_${NET}"
-DEST_CONF="${NOD_DIR}/conf/config.ini"
-mkdir -p $NOD_DIR/conf $NOD_DIR/data $NOD_DIR/logs
+DEST_HOME="${NODE_HOME}/amax_${NET}"
+DEST_CONF="${DEST_HOME}/conf/config.ini"
+mkdir -p $DEST_HOME/conf $DEST_HOME/data $DEST_HOME/logs
 
-cd ./amnod
-cp -r   ./bin                   $NOD_DIR/
-cp      ./$NET/genesis.json     $NOD_DIR/conf/
-cp      ./conf/conf_base.ini    $DEST_CONF
+cp -r   $CONF_DIR/bin                   $DEST_HOME/         && \
+cp      $CONF_DIR/genesis.json          $DEST_HOME/conf/    && \
+cp      $CONF_DIR/conf/conf_base.ini    $DEST_CONF
 
 # copy conf node info into config
 echo " " >> $DEST_CONF
 echo "#### Node base conf: " >> $DEST_CONF
-cat ./conf/conf_node.ini >> $DEST_CONF
+cat $CONF_DIR/conf/conf_node.ini >> $DEST_CONF
 
 if  [ "${history_plugin}" == "true" ]; then
     echo " " >> $DEST_CONF
     echo "#### History plugin conf: " >> $DEST_CONF
-    cat ./conf/conf_plugin_history.ini >> $DEST_CONF
+    cat $CONF_DIR/conf/conf_plugin_history.ini >> $DEST_CONF
 fi
 
 if  [ "${state_plugin}" == "true" ]; then
     echo " " >> $DEST_CONF
     echo "#### State plugin conf: " >> $DEST_CONF
-    cat ./conf/conf_plugin_state.ini >> $DEST_CONF
+    cat $CONF_DIR/conf/conf_plugin_state.ini >> $DEST_CONF
 fi
 
 if  [ "${bp_plugin}" == "true" ]; then
     echo " " >> $DEST_CONF
     echo "#### Block producer plugin conf: " >> $DEST_CONF
-    cat ./conf/conf_plugin_bp.ini >> $DEST_CONF
+    cat $CONF_DIR/conf/conf_plugin_bp.ini >> $DEST_CONF
 fi
 
 cd $NET && docker-compose up -d
