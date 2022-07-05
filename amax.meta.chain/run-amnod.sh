e4f3852927fdf8fdf2b2001a13bd567e79717948
masteray <masteraychen@gmail.com>
2022-07-05 15:45:02 +0800

    separate setup from runtime

--- amax.meta.chain/run-amnod.sh
+++ amax.meta.chain/run-amnod.sh
@@ -1,25 +1,8 @@
-NET=$1
-tag=$2
 
-CONF_DIR=~/.amax_${NET}_${tag}
-mkdir -p $CONF_DIR
+[ ! -f ./amnod.env ] && echo "not in amnod env, hence existing ..." && exit 1
 
-[ ! -f "$CONF_DIR/amnod.env" ]                          && \
-    cp      ./amnod/$NET/amnod.env      $CONF_DIR/      && \
-    cp      ./amnod/$NET/genesis.json   $CONF_DIR/      && \
-    cp      ./amnod/docker-compose.yml  $CONF_DIR/      && \
-    cp -r   ./amnod/conf                $CONF_DIR/      && \
-    cp -r   ./amnod/bin                 $CONF_DIR/
-    
-echo "NET=$NET" >> $CONF_DIR/amnod.env
-echo "tag=$tag" >> $CONF_DIR/amnod.env
-set -a && source $CONF_DIR/amnod.env
+source ./amnod.env
 
-DEST_HOME="${NODE_HOME}/amax_${NET}"
-DEST_CONF="${DEST_HOME}/conf/config.ini"
-mkdir -p $DEST_HOME/conf $DEST_HOME/data $DEST_HOME/logs
-
-cd $CONF_DIR
 cp -r   ./bin                   $DEST_HOME/         && \
 cp      ./genesis.json          $DEST_HOME/conf/    && \
 cp      ./conf/conf_base.ini    $DEST_CONF
---
+++ amax.meta.chain/setup-amnod.sh
@@ -0,0 +1,20 @@
+NET=$1
+tag=$2
+
+CONF_DIR=~/.amax_${NET}_${tag}
+mkdir -p $CONF_DIR
+
+[ ! -f "$CONF_DIR/amnod.env" ]                          && \
+    cp      ./amnod/$NET/amnod.env      $CONF_DIR/      && \
+    cp      ./amnod/$NET/genesis.json   $CONF_DIR/      && \
+    cp      ./amnod/docker-compose.yml  $CONF_DIR/      && \
+    cp -r   ./amnod/conf                $CONF_DIR/      && \
+    cp -r   ./amnod/bin                 $CONF_DIR/
+    
+echo "NET=$NET" >> $CONF_DIR/amnod.env
+echo "tag=$tag" >> $CONF_DIR/amnod.env
+set -a && source $CONF_DIR/amnod.env
+
+DEST_HOME="${NODE_HOME}/amax_${NET}"
+DEST_CONF="${DEST_HOME}/conf/config.ini"
+mkdir -p $DEST_HOME/conf $DEST_HOME/data $DEST_HOME/logs
