2aaf39b425b7e8ba80e6b1fa36f966292934b627
masteray <masteraychen@gmail.com>
2022-07-05 15:46:35 +0800

    update

--- amax.meta.chain/setup-amnod.sh
+++ amax.meta.chain/setup-amnod.sh
@@ -18,3 +18,5 @@ set -a && source $CONF_DIR/amnod.env
 DEST_HOME="${NODE_HOME}/amax_${NET}"
 DEST_CONF="${DEST_HOME}/conf/config.ini"
 mkdir -p $DEST_HOME/conf $DEST_HOME/data $DEST_HOME/logs
+cp ./run-amnod.sh $CONF_DIR/run.sh
+chmod +x $CONF_DIR/run.sh
