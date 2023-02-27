7ff264c62515f40549b79a545d225de0b53acbda
metamaster2021 <aws4amazon@163.com>
2023-02-27 16:01:00 +0800

    change amax.meta.chain version to v0.6.0.develp

--- amax.meta.chain/buildbase/build-amnod-unipub-img.sh
+++ amax.meta.chain/buildbase/build-amnod-unipub-img.sh
@@ -1,1 +1,3 @@
-docker build -t armoniax/amnod:0.5.0.unipub -f ./Dockerfile.unipub .
+#!/bin/bash
+# docker build -t armoniax/amnod:0.5.0.unipub -f ./Dockerfile.unipub .
+docker build -t armoniax/metamaster2021:0.6.0.develop .
