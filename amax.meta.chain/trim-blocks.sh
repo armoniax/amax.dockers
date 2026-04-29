ba567d87bbb7f8d0d9951f237a232622501bd88c
Web3BUIDL <76104526+masterayman@users.noreply.github.com>
2026-04-29 13:40:18 +0800

    Create trim-blocks.sh for block trimming and testing
    Added a script to trim blocks and test the effect.

---
+++ amax.meta.chain/trim-blocks.sh
@@ -0,0 +1,12 @@
+IMG=ghcr.io/armoniax/amnod:1.0.4
+H=127602651 ## modify this number to what you like
+
+echo "### Triming blocks now"
+docker run --rm -it \
+  -v /data/amax_mainnet_bps/data:/data/amax_mainnet_bps/data $IMG \
+  amax-blocklog --blocks-dir /data/amax_mainnet_bps/data/blocks --trim-blocklog --first $H
+
+echo "### Testing trim effectness"
+docker run --rm -it \
+  -v /data/amax_mainnet_bps/data:/data/amax_mainnet_bps/data $IMG \
+  amax-blocklog --blocks-dir /data/amax_mainnet_bps/data/blocks --smoke-test
